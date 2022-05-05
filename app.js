aa9f54eb73cbf3e673e0f9ca5f9a48f6a0733dfd
Noah Hradek <Noah.Hradek1@T-Mobile.com>
2022-05-05 16:02:11 -0600

    files

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const app = express();
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+let books = [];
+let id = 1;
 
 app.get("/health", (req, res) => {
   res.status(200).send("Don't panic.");
 });
 
 
+app.post("/api/books", (req, res) => {
+  const book = req.body;
+  book.id = id;
+  books.push(book);
+  id++;
+  res.status(201).send(book);
+});
+
+app.get("/api/books", (req, res) => {
+  books.sort((a, b) => (a.title > b.title) ? 1 : -1);
+  res.status(200).send(books);
+});
+
+app.delete("/api/books", (req, res) => {
+  books = [];
+  res.status(204).send();
+});
 
 module.exports = app;

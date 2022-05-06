3f4fdec2c7c8e489b27bdeb983c074aa62c525fa
Noah Hradek <Noah.Hradek1@T-Mobile.com>
2022-05-06 08:53:13 -0600

    changed port file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const port = 5000;
 const app = require('./app');
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Server is running, listening on port ${port}`);
 });
